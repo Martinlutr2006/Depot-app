7cdde1ddff021ef9e4d6fe19faa8e4d51f74a4d1
Martinlutr2006 <martinlutr17@gmail.com>
2025-05-09 09:32:41 +0200

    mboo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { BrowserRouter, Route } from "react-router-dom"
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<LoginPage}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
